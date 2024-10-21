992b9aca2952c1b0bacc88e3ff6807f63d7e6770
StepanSK <stephanskriabin@gmail.com>
2024-10-21 11:30:25 +0700

    perf: add section-container class to header element and import SectionContent

--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -1,28 +1,34 @@
 import styles from "./Header.module.css";
 import logo from "../../assets/images/logo.svg";
+import SectionContent from "../../components/SectionContent/SectionContent";
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="logo" className={styles.logo} />
-      <ul className={styles.links}>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About Us</a>
-        </li>
-        <li>
-          <a href="#">Properties</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
-      </ul>
+    <header className="section-container">
+      <SectionContent>
+        <a href="/">
+          <img src={logo} alt="logo" className={styles.logo} />
+        </a>
 
-      <a href="#" role="button" type="button" className={styles.button}>
-        Contact Us
-      </a>
+        <ul className={styles.links}>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Properties</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+        </ul>
+
+        <a href="#" role="button" type="button" className={styles.button}>
+          Contact Us
+        </a>
+      </SectionContent>
     </header>
   );
 }
