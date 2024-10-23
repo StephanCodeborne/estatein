4895fa67099eee98eb1954bbaec97a41f2841a34
StepanSK <stephanskriabin@gmail.com>
2024-10-23 11:45:54 +0700

    chore: Hero folder setup

---
+++ src/sections/Hero/Hero.js
@@ -0,0 +1,10 @@
+import SectionComponent from "../../components/SectionContent/SectionContent";
+import styles from "./Hero.module.css";
+
+export default function Hero() {
+  return (
+    <section id="hero" className="section-container">
+      <SectionComponent></SectionComponent>
+    </section>
+  );
+}
