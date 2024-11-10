8fe6b3a64a642d9e5be1e1ae8571ffef895909e0
StepanSK <stephanskriabin@gmail.com>
2024-11-10 11:52:42 +0700

    feat: add background image to the PromoBanner

--- src/sections/PromoBanner/PromoBanner.js
+++ src/sections/PromoBanner/PromoBanner.js
@@ -1,8 +1,9 @@
 import styles from "./PromoBanner.module.css";
+import abstractShape from '../../assets/images/abstract-shape-promo-banner.svg';
 
 export default function PromoBanner() {
   return (
-    <div className={styles.banner}>
+    <div className={styles.banner} style={{background: `url(${abstractShape}) center /cover no-repeat var(--grey-2)`}}>
       <div className={styles.text}>
         <p>✨Discover Your Dream Property with Estatein</p>
         <a href="#">Learn More</a>
