58f177f76e1b9ed40ff5b60b9dd305cf0873b641
StepanSK <stephanskriabin@gmail.com>
2024-11-10 10:56:42 +0700

    feat: add PromoBanner component to the App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./sections/Header/Header";
 import Hero from "./sections/Hero/Hero";
+import PromoBanner from "./sections/PromoBanner/PromoBanner";
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <Hero />
-    </>
-  );
+    return (
+        <>
+            <PromoBanner />
+            <Header />
+            <Hero />
+        </>
+    );
 }
