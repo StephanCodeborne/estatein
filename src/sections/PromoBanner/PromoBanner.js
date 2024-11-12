db24930d3a71cf6b5f8de2545db43677be3e5c46
StepanSK <stephanskriabin@gmail.com>
2024-11-12 20:37:53 +0700

    feat: add functionality to the PromoBanner

--- src/sections/PromoBanner/PromoBanner.js
+++ src/sections/PromoBanner/PromoBanner.js
@@ -1,18 +1,26 @@
 import styles from "./PromoBanner.module.css";
-import abstractShape from '../../assets/images/abstract-shape-promo-banner.svg';
-import cross from '../../assets/icons/cross.svg';
+import abstractShape from "../../assets/images/abstract-shape-promo-banner.svg";
+import cross from "../../assets/icons/cross.svg";
+import { useState } from "react";
 
 export default function PromoBanner() {
-    return (
-        <div className={styles.banner} style={{background: `url(${abstractShape}) center /cover no-repeat var(--grey-2)`}}>
-            <div className={styles.text}>
-                <p>✨Discover Your Dream Property with Estatein</p>
-                <a href="#">Learn More</a>
-            </div>
+  const [isOpen, setIsOpen] = useState(true);
 
-            <button className={styles.closeBanner}>
-                <img src={cross} alt="close"/>
-            </button>
-        </div>
-    );
+  return isOpen ? (
+    <div
+      className={styles.banner}
+      style={{
+        background: `url(${abstractShape}) center /cover no-repeat var(--grey-2)`,
+      }}
+    >
+      <div className={styles.text}>
+        <p>✨Discover Your Dream Property with Estatein</p>
+        <a href="#">Learn More</a>
+      </div>
+
+      <button className={styles.closeBanner} onClick={() => setIsOpen(false)}>
+        <img src={cross} alt="close" />
+      </button>
+    </div>
+  ) : null;
 }
