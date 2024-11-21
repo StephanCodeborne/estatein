a3caea4fa12483e635b948aea3df1f13ef055b52
StephanCodeborne <stephanskriabin@gmail.com>
2024-11-21 09:51:35 +0100

    refactor: reaplce list elements with Service Component

--- src/sections/Services/Services.js
+++ src/sections/Services/Services.js
@@ -3,64 +3,24 @@ import service1 from "../../assets/icons/service-1.svg";
 import service2 from "../../assets/icons/service-2.svg";
 import service3 from "../../assets/icons/service-3.svg";
 import service4 from "../../assets/icons/service-4.svg";
+import Service from "./Service/Service";
 
 export default function Services() {
     return (
         <section id="services" className={styles.container}>
             <ul className={styles.services}>
-                <li className={styles.service}>
-                    <a href="#">
-                        <div className={styles.text}>
-                            <img
-                                src={service2}
-                                alt="service icon"
-                                className={styles.icon}
-                            />
-                            <h4>Unlock Property Value</h4>
-                        </div>
-
-                        {/*<img src={arrow} alt="arrow" className={styles.arrow} />*/}
-                        <div className={styles.arrow}>
-                            <Arrow />
-                        </div>
-                    </a>
-                </li>
-
-                <li className={styles.service}>
-                    <a href="#">
-                        <div className={styles.text}>
-                            <img
-                                src={service3}
-                                alt="service icon"
-                                className={styles.icon}
-                            />
-                            <h4>Effortless Property Management</h4>
-                        </div>
-
-                        {/*<img src={arrow} alt="arrow" className={styles.arrow} />*/}
-                        <div className={styles.arrow}>
-                            <Arrow />
-                        </div>
-                    </a>
-                </li>
-
-                <li className={styles.service}>
-                    <a href="#">
-                        <div className={styles.text}>
-                            <img
-                                src={service4}
-                                alt="service icon"
-                                className={styles.icon}
-                            />
-                            <h4>Smart Investments, Informed Decisions</h4>
-                        </div>
-
-                        {/*<img src={arrow} alt="arrow" className={styles.arrow} />*/}
-                        <div className={styles.arrow}>
-                            <Arrow />
-                        </div>
-                    </a>
-                </li>
+                <Service icon={service1} href="#">
+                    Find Your Dream Home
+                </Service>
+                <Service icon={service2} href="#">
+                    Unlock Property Value
+                </Service>
+                <Service icon={service3} href="#">
+                    Effortless Property Management
+                </Service>
+                <Service icon={service4} href="#">
+                    Smart Investments, Informed Decisions
+                </Service>
             </ul>
         </section>
     );
