1f59b3f7dc43d49cdc7635d4b067e2c257c13856
StephanCodeborne <stephanskriabin@gmail.com>
2024-11-25 10:15:16 +0100

    feat: add JSX structure to the CommonContent component and import all necessary files

---
+++ src/components/CommonContent/CommonContent.js
@@ -0,0 +1,15 @@
+import styles from "./CommonContent.module.css";
+import oversectionIcon from "../../assets/icons/oversection-icon.svg";
+
+export default function CommonContent({ className, children }) {
+    return (
+        <div className={styles.content}>
+            <img
+                className={styles.icon}
+                src={oversectionIcon}
+                alt="abstract stars"
+            />
+            {children}
+        </div>
+    );
+}
