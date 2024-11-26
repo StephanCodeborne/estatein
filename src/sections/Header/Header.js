7585c09bde4a352eef2d896af2560cbcc0256e82
StephanCodeborne <stephanskriabin@gmail.com>
2024-11-26 09:27:01 +0100

    refactor: rename SectionContent component to InnerContainer

--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -1,35 +1,35 @@
 import styles from "./Header.module.css";
 import logo from "../../assets/images/logo.svg";
-import SectionContent from "../../components/SectionContent/SectionContent";
+import InnerContainer from "../../components/InnerContainer/InnerContainer";
 import Button from "../../components/Button/Button";
 
 export default function Header() {
-  return (
-    <header className="section-container">
-      <SectionContent className={styles.content}>
-        <a href="/">
-          <img src={logo} alt="logo" className={styles.logo} />
-        </a>
+    return (
+        <header className="section-container">
+            <InnerContainer className={styles.content}>
+                <a href="/">
+                    <img src={logo} alt="logo" className={styles.logo} />
+                </a>
 
-        <ul className={styles.links}>
-          <li>
-            <a className={styles.active} href="/">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="/">About Us</a>
-          </li>
-          <li>
-            <a href="/">Properties</a>
-          </li>
-          <li>
-            <a href="/">Services</a>
-          </li>
-        </ul>
+                <ul className={styles.links}>
+                    <li>
+                        <a className={styles.active} href="/">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/">About Us</a>
+                    </li>
+                    <li>
+                        <a href="/">Properties</a>
+                    </li>
+                    <li>
+                        <a href="/">Services</a>
+                    </li>
+                </ul>
 
-        <Button>contact us</Button>
-      </SectionContent>
-    </header>
-  );
+                <Button>contact us</Button>
+            </InnerContainer>
+        </header>
+    );
 }
