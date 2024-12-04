ecd90ac7ab461eaec10451fd4444ac39a84d056f
StephanCodeborne <stephanskriabin@gmail.com>
2024-12-04 09:30:07 +0100

    feat: write JSX structure for Properties component

--- src/components/Properties/Properties.js
+++ src/components/Properties/Properties.js
@@ -1,5 +1,11 @@
 import styles from "./Properties.module.css";
 
-export default function Properties() {
-    return null;
+export default function Properties({properties}) {
+    return (
+		<ul>
+			{properties.map((property, index) => ())}
+		</ul>
+    );
 }
+
+
