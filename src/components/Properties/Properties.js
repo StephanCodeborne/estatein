30b307a01eef3d339850ce3009d84bf8fa5b73ec
StephanCodeborne <stephanskriabin@gmail.com>
2024-12-04 09:54:24 +0100

    feat: pass Property component into the map function

--- src/components/Properties/Properties.js
+++ src/components/Properties/Properties.js
@@ -1,52 +1,59 @@
 import styles from "./Properties.module.css";
 
-export default function Properties({properties}) {
+export default function Properties({ properties }) {
     return (
-		<ul className={styles.properties}>
-			{properties.map((property, index) => ())}
-		</ul>
+        <ul className={styles.properties}>
+            {properties.map((property, index) => (
+                <Property
+                    key={index}
+                    image={property.image}
+                    title={property.title}
+                    href={property.href}
+                    description={property.description}
+                    info={property.info}
+                    price={property.price}
+                />
+            ))}
+        </ul>
     );
 }
 
-function Property({image, title, description, info, price, href}) {
-	return (<li className={styles.card}>
-		<img
-			src={image}
-			alt="property image"
-			className={styles.image}
-		/>
+function Property({ image, title, description, info, price, href }) {
+    return (
+        <li className={styles.card}>
+            <img src={image} alt="property image" className={styles.image} />
 
-		<div className={styles.text}>
-			<h3>{title}</h3>
-			<p>
-				{description}{" "}
-				<a href="#">Read More</a>
-			</p>
-		</div>
+            <div className={styles.text}>
+                <h3>{title}</h3>
+                <p>
+                    {description} <a href="#">Read More</a>
+                </p>
+            </div>
 
-		<div className={styles.info}>
-            <span>
-                <img src={bed} alt="bed"/>
-	            {info.bedroom}-Bedroom
-            </span>
-			<span>
-                <img src={bathroom} alt="bathroom"/>
-				{info.bathroom}-Bathroom
-            </span>
-			<span>
-                <img src={villa} alt="villa"/>
-				{info.propertyType}
-            </span>
-		</div>
+            <div className={styles.info}>
+                <span>
+                    <img src={bed} alt="bed" />
+                    {info.bedroom}-Bedroom
+                </span>
+                <span>
+                    <img src={bathroom} alt="bathroom" />
+                    {info.bathroom}-Bathroom
+                </span>
+                <span>
+                    <img src={villa} alt="villa" />
+                    {info.propertyType}
+                </span>
+            </div>
 
-		<div className={styles.footer}>
-			<div className={styles.price}>
-				<p className={styles.caption}>Price</p>
-				<h3>{price}</h3>
-			</div>
-			<Button href={href} variant="cta">
-				View Property Details
-			</Button>
-		</div>
-	</li>)
+            <div className={styles.footer}>
+                <div className={styles.price}>
+                    <p className={styles.caption}>Price</p>
+                    <h3>{price}</h3>
+                </div>
+                <Button href={href} variant="cta">
+                    View Property Details
+                </Button>
+            </div>
+        </li>
+    );
 }
