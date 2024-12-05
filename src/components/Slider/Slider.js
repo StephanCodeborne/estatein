deea6b1aa7f859c9c42d63754ce1418942a62257
StephanCodeborne <stephanskriabin@gmail.com>
2024-12-05 09:24:07 +0100

    feat: write JSX structure for Slider.js

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,5 +1,25 @@
-import styles from './Slider.module.css'
+import styles from "./Slider.module.css";
+import Properties from "../Properties/Properties";
+import Button from "../Button/Button";
 
-export default function Slider() {
-	return ()
+export default function Slider({ onNext, onPrev, children }) {
+    return (
+        <div className={styles.slider}>
+            {children}
+
+            <div className={styles.tools}>
+                <div className={styles.slideNumber}>
+                    <span className={styles.currentSlide}>01</span> of 10
+                </div>
+                <div className={styles.arrows}>
+                    <Button tag="button" onClick={onPrev} disabled={true}>
+                        <Arrow />
+                    </Button>
+                    <Button tag="button" onClick={onNext}>
+                        <Arrow />
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 }
