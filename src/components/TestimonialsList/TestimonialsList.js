9064148ece1991c88b5e4cc2abdcfe8ceb60b0ac
StephanCodeborne <stephanskriabin@gmail.com>
2024-12-12 10:21:58 +0100

    feat: write basiv JSX structure for TestimonialsList.js

--- src/components/TestimonialsList/TestimonialsList.js
+++ src/components/TestimonialsList/TestimonialsList.js
@@ -1,5 +1,7 @@
 import styles from "./TestimonialsList.module.css";
 
-export default function TestimonialsList() {
-    return <ul></ul>;
+export default function TestimonialsList({testimonials}) {
+    return <ul className={styles.testimonials}>
+	    {testimonials.map((testimonial, index) => ())}
+    </ul>;
 }
