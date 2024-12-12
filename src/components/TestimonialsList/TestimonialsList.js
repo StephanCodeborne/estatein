2e7e6081b3698ee538617dd1f8e53a241c9a5a1f
StephanCodeborne <stephanskriabin@gmail.com>
2024-12-12 10:26:49 +0100

    feat: create TestimonialCard Component and write JSX structure for it

--- src/components/TestimonialsList/TestimonialsList.js
+++ src/components/TestimonialsList/TestimonialsList.js
@@ -1,7 +1,59 @@
 import styles from "./TestimonialsList.module.css";
+import clientAvatar1 from "../../assets/images/client-avatar1.png";
 
 export default function TestimonialsList({testimonials}) {
     return <ul className={styles.testimonials}>
 	    {testimonials.map((testimonial, index) => ())}
     </ul>;
+}
+
+
+function TestimonialCard({ title, description, client }) {
+	return (
+		<li className={styles.card}>
+			<div className={styles.stars}>
+				<div className={styles.star}>
+					<Star alt="full star"/>
+				</div>
+				<div className={styles.star}>
+					<Star alt="full star"/>
+				</div>
+				<div className={styles.star}>
+					<Star alt="full star"/>
+				</div>
+				<div className={styles.star}>
+					<Star alt="full star"/>
+				</div>
+				<div className={styles.star}>
+					<Star alt="full star"/>
+				</div>
+			</div>
+
+			<div className={styles.text}>
+				<h3 className={styles.title}>
+					{title}
+				</h3>
+				<p className={styles.description}>
+					{description}
+				</p>
+			</div>
+
+			<div className={styles.client}>
+				<img
+					src={client.avatar}
+					alt="avatar"
+					className={styles.avatar}
+				/>
+
+				<div className={styles.info}>
+					<h4 className={styles.name}>
+						{client.name}
+					</h4>
+					<p className={styles.location}>
+						{client.location}
+					</p>
+				</div>
+			</div>
+		</li>
+	)
 }
