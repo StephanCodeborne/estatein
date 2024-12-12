92d9cd87f57372ad616c6ae1ee871e8fd7b5937b
StephanCodeborne <stephanskriabin@gmail.com>
2024-12-12 10:20:26 +0100

    refactor: rename Reviews component to TestimonialsList component

--- src/components/Reviews/Reviews.js
+++
@@ -1,5 +0,0 @@
-import styles from "./Reviews.module.css";
-
-export default function Reviews() {
-    return null;
-}
---
+++ src/components/TestimonialsList/TestimonialsList.js
@@ -0,0 +1,5 @@
+import styles from "./TestimonialsList.module.css";
+
+export default function TestimonialsList() {
+    return <ul></ul>;
+}
