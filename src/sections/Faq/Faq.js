86d464499db4e84baa1047bb98ca26cf5cb230d7
StephanCodeborne <stephanskriabin@gmail.com>
2024-12-18 09:24:57 +0100

    feat: wrap faq list in Slider component

--- src/sections/Faq/Faq.js
+++ src/sections/Faq/Faq.js
@@ -3,6 +3,7 @@ import InnerContainer from "../../components/InnerContainer/InnerContainer";
 import CommonContent from "../../components/CommonContent/CommonContent";
 import IntroText from "../../components/IntroText/IntroText";
 import Button from "../../components/Button/Button";
+import Slider from "../../components/Slider/Slider";
 
 export default function Faq() {
     return (
@@ -21,32 +22,43 @@ export default function Faq() {
                         every step of the way.
                     </IntroText>
 
-                    <ul className={styles.faq}>
-                        <li className={styles.card}>
-                            <h3>How do I search for properties on Estatein?</h3>
-                            <p>
-                                Learn how to use our user-friendly search tools
-                                to find properties that match your criteria.
-                            </p>
-                            <Button>Read More</Button>
-                        </li>
-                        <li className={styles.card}>
-                            <h3>How do I search for properties on Estatein?</h3>
-                            <p>
-                                Learn how to use our user-friendly search tools
-                                to find properties that match your criteria.
-                            </p>
-                            <Button>Read More</Button>
-                        </li>
-                        <li className={styles.card}>
-                            <h3>How do I search for properties on Estatein?</h3>
-                            <p>
-                                Learn how to use our user-friendly search tools
-                                to find properties that match your criteria.
-                            </p>
-                            <Button>Read More</Button>
-                        </li>
-                    </ul>
+                    <Slider length={9}>
+                        <ul className={styles.faq}>
+                            <li className={styles.card}>
+                                <h3>
+                                    How do I search for properties on Estatein?
+                                </h3>
+                                <p>
+                                    Learn how to use our user-friendly search
+                                    tools to find properties that match your
+                                    criteria.
+                                </p>
+                                <Button>Read More</Button>
+                            </li>
+                            <li className={styles.card}>
+                                <h3>
+                                    How do I search for properties on Estatein?
+                                </h3>
+                                <p>
+                                    Learn how to use our user-friendly search
+                                    tools to find properties that match your
+                                    criteria.
+                                </p>
+                                <Button>Read More</Button>
+                            </li>
+                            <li className={styles.card}>
+                                <h3>
+                                    How do I search for properties on Estatein?
+                                </h3>
+                                <p>
+                                    Learn how to use our user-friendly search
+                                    tools to find properties that match your
+                                    criteria.
+                                </p>
+                                <Button>Read More</Button>
+                            </li>
+                        </ul>
+                    </Slider>
                 </CommonContent>
             </InnerContainer>
         </section>
