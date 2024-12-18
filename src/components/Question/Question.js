7c99f1adec249c3b0a05596ee9ff1b7d64383876
StephanCodeborne <stephanskriabin@gmail.com>
2024-12-18 09:49:07 +0100

    feat: use props in JSX

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -4,11 +4,8 @@ import Button from "../Button/Button";
 export default function Question({ question, description, href }) {
     return (
         <li className={styles.question}>
-            <h3>How do I search for properties on Estatein?</h3>
-            <p>
-                Learn how to use our user-friendly search tools to find
-                properties that match your criteria.
-            </p>
+            <h3>{question}</h3>
+            <p>{description}</p>
             <Button href={href}>Read More</Button>
         </li>
     );
