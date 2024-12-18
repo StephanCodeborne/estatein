658be3094154d24b02e625322be7a72a90c71913
StephanCodeborne <stephanskriabin@gmail.com>
2024-12-18 09:42:41 +0100

    feat: write JSX structure and props

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,5 +1,15 @@
 import styles from "./Question.module.css";
+import Button from "../Button/Button";
 
-export default function Question(props) {
-    return null;
+export default function Question({ question, description, href }) {
+    return (
+        <li className={styles.question}>
+            <h3>How do I search for properties on Estatein?</h3>
+            <p>
+                Learn how to use our user-friendly search tools to find
+                properties that match your criteria.
+            </p>
+            <Button href={href}>Read More</Button>
+        </li>
+    );
 }
