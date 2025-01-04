bf61250c9b07c5bb1f9d7fedcb99d874fc97232a
StephanCodeborne <stephanskriabin@gmail.com>
2025-01-04 10:05:46 +0100

    feat: write JSX structure for Footer.js

--- src/sections/Footer/Footer.js
+++ src/sections/Footer/Footer.js
@@ -1,5 +1,126 @@
 import styles from "./Footer.module.css";
+import InnerContainer from "../../components/InnerContainer/InnerContainer";
 
 export default function Footer() {
-    return null;
+    return (
+        <footer className="section-container">
+            <InnerContainer className={styles.innerContainer}>
+                <form className={styles.form}>
+                    <img src="" alt="estatein logo" className={styles.logo} />
+
+                    <div className={styles.inputContainer}>
+                        <input
+                            type="email"
+                            className={styles.email}
+                            name="email"
+                            placeholder="Enter Your Email"
+                        />
+                    </div>
+                </form>
+
+                <div className={styles.linksContainer}>
+                    <div className={styles.links}>
+                        <h4>Home</h4>
+                        <ul className={styles.links}>
+                            <li>
+                                <a href="#">Hero Section</a>
+                            </li>
+                            <li>
+                                <a href="#">Features</a>
+                            </li>
+                            <li>
+                                <a href="#">Properties</a>
+                            </li>
+                            <li>
+                                <a href="#">Testimonials</a>
+                            </li>
+                            <li>
+                                <a href="#">FAQ’s</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.links}>
+                        <h4>Home</h4>
+                        <ul className={styles.links}>
+                            <li>
+                                <a href="#">Hero Section</a>
+                            </li>
+                            <li>
+                                <a href="#">Features</a>
+                            </li>
+                            <li>
+                                <a href="#">Properties</a>
+                            </li>
+                            <li>
+                                <a href="#">Testimonials</a>
+                            </li>
+                            <li>
+                                <a href="#">FAQ’s</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.links}>
+                        <h4>Home</h4>
+                        <ul className={styles.links}>
+                            <li>
+                                <a href="#">Hero Section</a>
+                            </li>
+                            <li>
+                                <a href="#">Features</a>
+                            </li>
+                            <li>
+                                <a href="#">Properties</a>
+                            </li>
+                            <li>
+                                <a href="#">Testimonials</a>
+                            </li>
+                            <li>
+                                <a href="#">FAQ’s</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.links}>
+                        <h4>Home</h4>
+                        <ul className={styles.links}>
+                            <li>
+                                <a href="#">Hero Section</a>
+                            </li>
+                            <li>
+                                <a href="#">Features</a>
+                            </li>
+                            <li>
+                                <a href="#">Properties</a>
+                            </li>
+                            <li>
+                                <a href="#">Testimonials</a>
+                            </li>
+                            <li>
+                                <a href="#">FAQ’s</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={styles.links}>
+                        <h4>Home</h4>
+                        <ul className={styles.links}>
+                            <li>
+                                <a href="#">Hero Section</a>
+                            </li>
+                            <li>
+                                <a href="#">Features</a>
+                            </li>
+                            <li>
+                                <a href="#">Properties</a>
+                            </li>
+                            <li>
+                                <a href="#">Testimonials</a>
+                            </li>
+                            <li>
+                                <a href="#">FAQ’s</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </InnerContainer>
+        </footer>
+    );
 }
